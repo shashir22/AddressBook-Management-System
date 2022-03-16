317f8b47f6298ea14cf8ec5d141b7fbcd2106685
Shashikumar <shashirgowdas67@gmail.com>
2022-03-16 21:30:34 +0530

    [Shashi] Add:Added CSV files for Read and Write Operation

---
+++ AddressBook-Management-System/Contacts.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook_Management_System
+{
+    public class Contacts
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Email { get; set; }
+        public int Zip { get; set; }
+        public long PhoneNumber { get; set; }
+        public Contacts(string firstName, string lastName, string address, string city, string state, string email, int zip, long phoneNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            City = city;
+            State = state;
+            Email = email;
+            Zip = zip;
+            PhoneNumber = phoneNumber;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Contacts contact = (Contacts)obj;
+            if (contact == null)
+                return false;
+            else
+                return FirstName.Equals(contact.FirstName) && LastName.Equals(contact.LastName);
+        }
+
+        public override string ToString()
+        {
+            return "First Name :" + FirstName + "\nLast Name : " + LastName + "\nCity : " + City + "\nState : " + State + "\nEmail : " + Email + "\nZip : " + Zip + "\nPhone Number : " + PhoneNumber + "\n";
+        }
+    }
+}
--- AddressBook-Management-System/Program.cs
+++ AddressBook-Management-System/Program.cs
@@ -7,49 +7,157 @@ namespace AddressBook_Management_System
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Address Book Program");
-            AddressBookDetails abd = new AddressBookDetails();
-            while (true)
+            int option, option1;
+            string bookName = "Default";
+            AddressBookBuider addressBook = new AddressBookBuider();
+
+            Console.WriteLine("1. Would you like to work on existing addressbook ? if yes press 1");
+            Console.WriteLine("2. Would you like to add  new addressbook ? if yes press 2");
+            option1 = Convert.ToInt32(Console.ReadLine());
+            switch (option1)
             {
-                Console.WriteLine("*********************************************************");
-                Console.WriteLine("1. Add member to Contact list \n2.View Members in Contact List\n3.Edit members Contacts lists\n4.Delete members Contacts list\n5.Search Details\n6.Count\n7.SortEntries\n8.ReadFromFile\n9.WriteToFile\n10.Exit");
-                Console.WriteLine("Enter an option:");
-                switch (Convert.ToInt32(Console.ReadLine()))
+                case 1:
+                    addressBook.AddAddressBook(bookName);
+                    break;
+                case 2:
+                    Console.WriteLine("Enter Name Of New Addressbook You want to create : ");
+                    bookName = Console.ReadLine();
+                    addressBook.AddAddressBook(bookName);
+                    break;
+            }
+            do
+            {
+                Console.WriteLine($"you are working on {bookName} addressbook ");
+                Console.WriteLine("Enter your option :");
+                Console.WriteLine("1. Add new contact ");
+                Console.WriteLine("2. Display the contacts");
+                Console.WriteLine("3. Edit  contacts");
+                Console.WriteLine("4. Delete  contacts");
+                Console.WriteLine("5. Add new addressbook");
+                Console.WriteLine("6. Switch Addressbook");
+                Console.WriteLine("7. Search person in a city or State");
+                Console.WriteLine("8. Get count of  persons by city or State");
+                Console.WriteLine("9. Sort Entries by Person name");
+                Console.WriteLine("10.Read or write addressbook contacts using File IO");
+                Console.WriteLine("11.Read or write addressbook contacts using CSV file");
+                option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
                 {
                     case 1:
-                        abd.AddMember();
+                        Console.WriteLine("Enter First Name :");
+                        string firstName = Console.ReadLine();
+                        Console.WriteLine("Enter Last Name :");
+                        string lastName = Console.ReadLine();
+                        Contacts duplicateCheck = new Contacts(firstName, lastName, null, null, null, null, 0, 0);
+                        if (addressBook.CheckFor_Duplicate(duplicateCheck, bookName))
+                        {
+                            break;
+                        }
+                        Console.WriteLine("Enter Address :");
+                        string address = Console.ReadLine();
+                        Console.WriteLine("Enter City :");
+                        string city = Console.ReadLine();
+                        Console.WriteLine("Enter State :");
+                        string state = Console.ReadLine();
+                        Console.WriteLine("Enter Email :");
+                        string email = Console.ReadLine();
+                        Console.WriteLine("Enter Zip :");
+                        int zip = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter Phone Number :");
+                        long phoneNumber = Convert.ToInt64(Console.ReadLine());
+                        addressBook.AddContact(firstName, lastName, address, city, state, email, zip, phoneNumber, bookName);
                         break;
                     case 2:
-                        abd.ViewContacts();
+                        addressBook.DisplayContact(bookName);
                         break;
                     case 3:
-                        abd.EditDetails();
+                        Console.WriteLine("Enter Full Name of contact which to be edited");
+                        string NameToEdit = Console.ReadLine();
+                        addressBook.EditContact(NameToEdit, bookName);
                         break;
+
                     case 4:
-                        abd.DeleteDetails();
+                        Console.WriteLine("Enter Full Name of contact which to be deleted");
+                        string NameToDelete = Console.ReadLine();
+                        addressBook.DeleteContact(NameToDelete, bookName);
                         break;
+
                     case 5:
-                        abd.SearchDetails();
+                        Console.WriteLine("Enter Name For New AddressBook");
+                        string newAddressBook = Console.ReadLine();
+                        addressBook.AddAddressBook(newAddressBook);
+                        Console.WriteLine("Would you like to Switch to " + newAddressBook);
+                        Console.WriteLine("1.Yes \n2.No");
+                        int c = Convert.ToInt32(Console.ReadLine());
+                        if (c == 1)
+                        {
+                            bookName = newAddressBook;
+                        }
                         break;
                     case 6:
-                        abd.CountByStateOrCity();
+                        Console.WriteLine("Enter Name Of AddressBook From Below List");
+                        foreach (KeyValuePair<string, AddressBookBuider> item in addressBook.GetAddressBook())
+                        {
+                            Console.WriteLine(item.Key);
+                        }
+                        while (true)
+                        {
+                            bookName = Console.ReadLine();
+                            if (addressBook.GetAddressBook().ContainsKey(bookName))
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("No such AddressBook found. Try Again.");
+                            }
+                        }
                         break;
                     case 7:
-                        abd.SortEntries();
+                        Console.WriteLine("Would You Like To \n1.Search by city \n2.Search by state");
+                        int opt = Convert.ToInt32(Console.ReadLine());
+                        switch (opt)
+                        {
+                            case 1:
+                                Console.WriteLine("Enter name of city :");
+                                addressBook.SearchPersonByCity(Console.ReadLine());
+                                break;
+                            case 2:
+                                Console.WriteLine("Enter name of state :");
+                                addressBook.SearchPersonByState(Console.ReadLine());
+                                break;
+                            default:
+                                Console.WriteLine("Invalid Input.Enter 1 or 2");
+                                break;
+                        }
                         break;
                     case 8:
-                        abd.ReadFromFile();
+                        addressBook.GetCountByCityOrState(bookName);
                         break;
                     case 9:
-                        abd.WriteToFile();
+                        addressBook.SortEntryByName();
                         break;
                     case 10:
-                        // to exit from main method
-                        Console.WriteLine("Exited");
-                        return;
-                }
+                        ReadWriteFileIO fileIO = new ReadWriteFileIO();
+                        fileIO.WriteToFile(addressBook.addressBookDictionary);
+                        fileIO.ReadFromFile();
+                        break;
+                    case 11:
+                        CSVHandler handler = new CSVHandler();
+                        handler.WriteToCsv(addressBook.addressBookDictionary);
+                        handler.ReadFromCSV();
 
+                        break;
+
+                    default:
+                        Console.WriteLine("wrong input");
+                        break;
+                }
+                Console.WriteLine("Do you want to continue to The Main Menu?  press 1 if yes,press 0 for Exit");
+                option = Convert.ToInt32(Console.ReadLine());
             }
+            while (option != 0);
         }
     }
 }
