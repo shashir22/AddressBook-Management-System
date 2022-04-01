60c177c653c933a521ee8d58463a3dc37ab407f4
Shashikumar <shashirgowdas67@gmail.com>
2022-04-01 15:36:01 +0530

    [Shashi]: Upadte contacts in the address book

---
+++ AddressBook-Management-System/AddressBookDatabases.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace AddressBook_DataBase
+{
+    public class AddressBookDatabases
+    {
+        string connectionString = @"Data Source=DESKTOP-DO2TJ68;Initial Catalog=AddressBookSystem;Integrated Security=True";
+
+        public int GetPersonDetailsfromDatabase()
+        {
+            int Count = 0;
+            try
+            {
+                AddressBookModel addressBookModel = new AddressBookModel();
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    string retrieveQuery = @"select p.person_id,p.firstname,p.lastname,p.phone,p.email,ab.book_name,ab.book_id,ab.book_type,p.zip ,a.city,a.state from person p inner join address a on p.zip=a.zip inner join Person_addressbook pa on pa.person_id=p.person_id inner join addressbook ab on ab.book_id=pa.book_id";
+                    ;
+                    SqlCommand sqlCommand = new SqlCommand(retrieveQuery, connection);
+                    connection.Open();
+                    SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+                    if (sqlDataReader.HasRows)
+                    {
+                        Console.WriteLine("Address Book Services Database has following Contact details right now");
+                        while (sqlDataReader.Read())
+                        {
+                            addressBookModel.person_id = sqlDataReader.GetInt32(0);
+                            addressBookModel.firstname = sqlDataReader.GetString(1);
+                            addressBookModel.lastname = sqlDataReader.GetString(2);
+                            addressBookModel.phone = sqlDataReader.GetString(3);
+                            addressBookModel.email = sqlDataReader.GetString(4);
+                            addressBookModel.book_id = sqlDataReader.GetInt32(6);
+                            addressBookModel.city = sqlDataReader.GetString(9);
+                            addressBookModel.zip = sqlDataReader.GetInt32(8);
+                            addressBookModel.state = sqlDataReader.GetString(10);
+                            addressBookModel.book_name = sqlDataReader.GetString(5);
+                            addressBookModel.book_type = sqlDataReader.GetString(7);
+                            Count++;
+                            Console.WriteLine("{0}, {1}, {2}, {4}, {5}, {6}, {7}, {8}, {9}, {10}", addressBookModel.person_id, addressBookModel.firstname, addressBookModel.lastname,
+                                addressBookModel.phone, addressBookModel.email, addressBookModel.book_id, addressBookModel.city, addressBookModel.zip, addressBookModel.state, addressBookModel.book_name, addressBookModel.book_type);
+                        }
+                        Console.WriteLine("New Contact Added Successfully");
+                        sqlDataReader.Close();
+                    }
+                    connection.Close();
+                }
+                return Count;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+        public bool UpdateContact(AddressBookModel model)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    SqlCommand command = new SqlCommand("SpUpdateContact", connection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@first_name", model.firstname);
+                    command.Parameters.AddWithValue("@last_name", model.lastname);
+                    command.Parameters.AddWithValue("@phone_no", model.phone);
+                    command.Parameters.AddWithValue("@email1", model.email);
+                    command.Parameters.AddWithValue("@zip1", model.zip);
+
+
+                    connection.Open();
+                    var result = command.ExecuteNonQuery();
+                    Console.WriteLine("Contact Updated Successfully !");
+                    connection.Close();
+                    if (result == 0)
+                    {
+                        return false;
+                    }
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
